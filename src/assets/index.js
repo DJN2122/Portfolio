c2e0bd54e9ec7a6401ddee98b27eac4aaad47156
David Niculita <david_niculita@yahoo.com>
2024-12-15 14:18:45 +0000

    Initial commit for portfolio update

---
+++ src/assets/index.js
@@ -0,0 +1,89 @@
+import bwmap from './backgrounds/bw-map.jpeg';
+import dublin from './backgrounds/dublin.png';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+
+import logo from './logo/djn.png';
+import logotext from './logo/DAVIDNICULITA.png';
+
+import shaq from './personal/david.png';
+
+import backend from './icons/backend.png';
+import ux from './icons/ux.png';
+import frontend from './icons/frontend.png';
+import prototyping from './icons/prototyping.png';
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import replay from './icons/replay.png';
+import replayHover from './icons/replayHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import postgresql from './tech/postgresql.png';
+
+import AIB from './company/AIB.png';
+import ERGO from './company/ERGO.png';
+import KS from './company/KS.png';
+import EY from './company/EY.png';
+
+import soundwave from './projects/soundwave.jpeg';
+import MedAssistLogo from './projects/MedAssistLogo.png';
+import math from './projects/SpaceInvader.png';
+import movie from './projects/livediary.jpeg.png';
+import ads from './projects/ads.png';
+
+export {
+  bwmap,
+  dublin,
+  whiteabstract,
+  worldmap,
+  logo,
+  logotext,
+  shaq,
+  backend,
+  ux,
+  frontend,
+  prototyping,
+  github,
+  close,
+  download,
+  downloadHover,
+  replay,
+  replayHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  nodejs,
+  postgresql,
+  reactjs,
+  tailwind,
+  typescript,
+  soundwave,
+  MedAssistLogo,
+  math,
+  movie,
+  ads,
+  AIB,
+  ERGO,
+  KS,
+  EY,
+};
